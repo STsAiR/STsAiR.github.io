1da2b865209bfc576eb684cef5d2f40b940b0095
STsAiR <94659937+STsAiR@users.noreply.github.com>
2023-12-29 15:53:58 +0800

    renew folder

--- JSTest/script.js
+++
@@ -1,7 +0,0 @@
-const tempDataValue = localStorage.getItem('temp');
-
-if (localStorage.getItem('name')) {
-  console.log('The data exists in localStorage');
-} else {
-  console.log('The data does not exist in localStorage');
-}
